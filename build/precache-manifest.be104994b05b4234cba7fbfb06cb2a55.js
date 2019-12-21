f1804ea87318bf74ec74cc4c09cdddfefa759dd4
NoLogicTheOne <monologic.loggy@yandex.ru>
2019-12-21 22:35:28 +0300

    build for easy start using templater

---
+++ build/precache-manifest.be104994b05b4234cba7fbfb06cb2a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856a77e8fc939589013097623b3da7c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa6a5ea38b5415d25bf",
+    "url": "/static/css/main.a5241eea.chunk.css"
+  },
+  {
+    "revision": "578d0a3099bf354f0370",
+    "url": "/static/js/2.e634e220.chunk.js"
+  },
+  {
+    "revision": "1aa6a5ea38b5415d25bf",
+    "url": "/static/js/main.09114cf6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
